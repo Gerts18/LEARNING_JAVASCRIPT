19d0242f8cce4b3b18f79b7d99535d42f6ab6a59
German <germants100@gmail.com>
2024-05-06 22:26:55 -0600

    Using onclick

--- aluraMidi/main.js
+++ aluraMidi/main.js
@@ -1,9 +1,14 @@
 // File responsible of giving dynamism to the main page
 
-document.querySelector('.tecla_pom'); //Bringing a html element by its class name
-
 // document.querySelector('#sonido_tecla_pom') //Bringing a html element by its id
 
 function playSonidoPom(){ //Using a function to execute my functionality 
     document.querySelector('#sonido_tecla_pom').play(); //using a functionality
 }
+
+function playSonidoClap(){
+    document.querySelector('#sonido_tecla_clap').play();
+}
+
+//Bringing a html element by its class name
+document.querySelector('.tecla_pom').onclick = playSonidoPom; //Using a function in an atributte
