c4b97c6b53f51e2e35fcb21cbde414ea1d7d85e9
German <germants100@gmail.com>
2024-05-06 20:08:20 -0600

    Applying concepts

--- Curso/timers.js
+++ Curso/timers.js
@@ -3,7 +3,18 @@
 window.addEventListener('load',() => {
     
     var tiempo = setInterval(() => {
+
+
         console.log('Intervalo Ejecutado')
+
+        var encabezado = document.querySelector('h1');
+
+        if(encabezado.style.fontSize == '50px'){
+            encabezado.style.fontSize = '30px'
+        }else{
+            encabezado.style.fontSize = '50px'
+        }
+
     }, 3000 );
 
 })
