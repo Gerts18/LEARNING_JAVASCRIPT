b2bcf9e6883934f8921d69a8ce467fccdf06e8bc
German <germants100@gmail.com>
2024-05-06 22:19:35 -0600

    Recap functions and other functionalities

--- aluraMidi/main.js
+++ aluraMidi/main.js
@@ -1,3 +1,9 @@
-// File resposible of giving dynamism to the main page
+// File responsible of giving dynamism to the main page
 
-document.querySelector('.tecla_pom');
+document.querySelector('.tecla_pom'); //Bringing a html element by its class name
+
+// document.querySelector('#sonido_tecla_pom') //Bringing a html element by its id
+
+function playSonidoPom(){ //Using a function to execute my functionality 
+    document.querySelector('#sonido_tecla_pom').play(); //using a functionality
+}
