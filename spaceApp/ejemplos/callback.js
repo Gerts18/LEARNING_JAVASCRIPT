2bc13649b87df2922ea25bfe0ce52c1e3ad3a3e9
German <germants100@gmail.com>
2024-06-25 10:00:57 -0600

    Using callbacks

---
+++ spaceApp/ejemplos/callback.js
@@ -0,0 +1,9 @@
+function uno () { //Synchronous function
+    console.log("uno")
+}
+
+function dos () { //Synchronous function
+    console.log("dos")
+}
+
+setTimeout(uno, 5000) //Asynchronous function
