6a4edfec50214bc803fc9a1e7cfb4dead72ac377
German <germants100@gmail.com>
2024-06-25 10:18:44 -0600

    Correct way to implement it

--- spaceApp/ejemplos/callback.js
+++ spaceApp/ejemplos/callback.js
@@ -1,13 +1,14 @@
-function uno () { //Synchronous function
+function uno (otherFunction) { //Synchronous function
     console.log("uno");
-    setTimeout(dos, 3000);
+    setTimeout(otherFunction, 3000);
 }
 
 function dos () { //Synchronous function
     console.log("dos");
 }
 
-setTimeout(() => uno(), 5000); //Asynchronous function
+setTimeout(() => uno(dos), 5000); //Asynchronous function
+// In this case I pass it as a parameter a function that will be accomplished later
 
 const nombre = function () { //Anonymous function
     console.log('nombre');
