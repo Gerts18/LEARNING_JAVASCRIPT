913a80d31f068bcb8b01314113770cf0784ea997
German <germants100@gmail.com>
2024-06-25 10:13:59 -0600

    Using arrow and anonymous functions

--- spaceApp/ejemplos/callback.js
+++ spaceApp/ejemplos/callback.js
@@ -1,9 +1,17 @@
 function uno () { //Synchronous function
-    console.log("uno")
+    console.log("uno");
+    setTimeout(dos, 3000);
 }
 
 function dos () { //Synchronous function
-    console.log("dos")
+    console.log("dos");
 }
 
-setTimeout(uno, 5000) //Asynchronous function
+setTimeout(() => uno(), 5000); //Asynchronous function
+
+const nombre = function () { //Anonymous function
+    console.log('nombre');
+};
+
+const apellido = () => console.log('apellido'); //Arrow function
+
