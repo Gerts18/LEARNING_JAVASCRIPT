c91095bf2754bf66d51d21a4f3a7b1bddacec605
German <germants100@gmail.com>
2024-06-26 10:43:05 -0600

    Using promises with then and catch

--- spaceApp/script.js
+++ spaceApp/script.js
@@ -1,3 +1,18 @@
 const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5";
 
-fetch(url)
+// Part of the code used to get data from an API
+let listaImagenes = () => {
+    fetch(url) //Accesing to the API
+    .then( //Used to handle the response if resolved.
+        response => response.json()
+    )
+    .then (
+        dataImages => {
+            console.log(dataImages)
+        }
+    )
+    .catch( //Used to handle the response if rejected.
+
+    )
+};
+
