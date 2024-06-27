58dc2cb836c03555a263fc47144d3e8a67e860c4
German <germants100@gmail.com>
2024-06-27 12:37:33 -0600

    adding an event

--- AluraPlay/js/crearVideo.js
+++ AluraPlay/js/crearVideo.js
@@ -1,10 +1,16 @@
 
 const formulario  = document.querySelector('[data-formulario]');
 
-function crearVideo(){
+function crearVideo(e){
+    e.preventDefault(); //Avoiding default execution of the form 
+
     const titulo = document.querySelector('[data-titulo]').value;
     const url = document.querySelector('[data-url]').value;
     const imagen = document.querySelector('[data-imagen]').value;
 
     const descripcion = Math.floor(Math.random * 10).toString();
 }
+
+formulario.addEventListener('submit', (evento) => {
+    crearVideo(evento);
+} )
