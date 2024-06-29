1b59d4a4dcebcacf35ae7d4df82e11472bf96811
German <germants100@gmail.com>
2024-06-29 12:26:44 -0600

    Adding events

---
+++ AluraBank/js/main.js
@@ -0,0 +1,11 @@
+const camposFormulario = document.querySelectorAll("[required]"); //Selecting and saving all elements HTML that have a required attribute 
+
+camposFormulario.forEach((campo) => {
+    campo.addEventListener('blur', () => {
+        verificarCampo(campo);
+    })
+});
+
+function verificarCampo (campo){
+    console.log("Cambio");
+}
