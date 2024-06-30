bf4cc23761ea288f5df28375e1c52ab39352901e
German <germants100@gmail.com>
2024-06-30 12:24:30 -0600

    Obtaining age

--- AluraBank/js/main.js
+++ AluraBank/js/main.js
@@ -1,4 +1,5 @@
 import esUnCuil from "./ValidarCuil.js"; //Importing my function
+import esMayorDeEdad from "./validarEdad.js";
 
 const camposFormulario = document.querySelectorAll("[required]"); //Selecting and saving all elements HTML that have a required attribute 
 
@@ -11,5 +12,8 @@ camposFormulario.forEach((campo) => { //Itereating each element and adding them 
 function verificarCampo (campo){ //Function asociated to the event
     if(campo.name == "cuil" && campo.value.length >= 11){
         esUnCuil(campo)
+    }
+    if(campo.name == "fecha_nacimiento" && campo.value != ""){
+        esMayorDeEdad(campo)
     }
 }
---
+++ AluraBank/js/validarEdad.js
@@ -0,0 +1,4 @@
+export default function esMayorDeEdad(campo){
+    const fechaNacimiento = new Date(campo.value);
+    console.log(fechaNacimiento)
+}
