85d82c6c9e4d778b2083ece0f7d41df6ac6eff25
German <germants100@gmail.com>
2024-06-30 12:28:58 -0600

    Validating age

--- AluraBank/js/validarEdad.js
+++ AluraBank/js/validarEdad.js
@@ -1,4 +1,11 @@
 export default function esMayorDeEdad(campo){
-    const fechaNacimiento = new Date(campo.value);
-    console.log(fechaNacimiento)
+    const fechaNacimiento = new Date(campo.value); // Creting a new object Date
+    console.log(validarEdad(fechaNacimiento));
 }
+
+function validarEdad(fecha){
+    const fechaActual = new Date();
+    const FechaMas18 = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(), fecha.getUTCDate());
+
+    return fechaActual >= FechaMas18;
+}   
