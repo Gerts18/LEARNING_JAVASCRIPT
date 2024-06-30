4e37339067e6cb2bbcc656d6d2dd2139c1823182
German <germants100@gmail.com>
2024-06-29 19:05:08 -0600

    Avoiding repeated numbers

--- AluraBank/js/ValidarCuil.js
+++ AluraBank/js/ValidarCuil.js
@@ -1,5 +1,23 @@
 export default function esUnCuil(campo){
-    const cuil = campo.value.replaced(/[-\/]/g, ""); //Replacing some characters 
-
+    const cuil = campo.value.replace(/[-\/]/g, ""); //Replacing some characters 
+    tieneNumerosRepetidos(cuil);
     console.log(cuil);
+    console.log(tieneNumerosRepetidos(cuil))
+}
+
+function tieneNumerosRepetidos(cuil){
+    const numerosRepetidos = [
+        "00000000000",
+        "11111111111",
+        "22222222222",
+        "33333333333",
+        "44444444444",
+        "55555555555",
+        "66666666666",
+        "77777777777",
+        "88888888888",
+        "99999999999",
+    ];
+
+    return numerosRepetidos.includes(cuil)
 }
