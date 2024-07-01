fc58e1ebf2b572f467e59cea4c5b72896c423dcb
German <germants100@gmail.com>
2024-07-01 13:34:22 -0600

    Taking a photo

--- AluraBank/js/camara.js
+++ AluraBank/js/camara.js
@@ -2,9 +2,21 @@ const botonAbrirCamara = document.querySelector("[data-video-boton]");
 const video = document.querySelector("[data-video]");
 const campoCamara = document.querySelector("[data-camera]");
 
+const botonTomarFoto = document.querySelector("[data-tomar-foto]");
+const mensaje = document.querySelector("[data-mensaje]");
+const canvas = document.querySelector("[data-video-canvas]");
+let imgUrl = "";
+
 botonAbrirCamara.addEventListener('click', async () => {
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true, audio:false}); //Accesing to the camera
-    botonAbrirCamara.style.display="none";
-    campoCamara.style.display = "block";
-    video.srcObject = iniciarVideo;
+    botonAbrirCamara.style.display="none"; //Hidding my element
+    campoCamara.style.display = "block"; //Showing my element 
+    video.srcObject = iniciarVideo; //Initializing an attribute 
+})
+
+botonTomarFoto.addEventListener("click", () => {
+    canvas.getContext("2d").drawImage(video,0,0,canvas.width,canvas.height); 
+    imgUrl = canvas.toDataURL("image/jpeg");
+    campoCamara.style.display = "none";
+    mensaje.style.display = "block";
 })
