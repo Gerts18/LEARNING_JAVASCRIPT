2a12fcd9bbd3c8e95ad3dddfd148b6ddf3715883
German <germants100@gmail.com>
2024-07-01 12:10:42 -0600

    Printing messages

--- AluraBank/js/main.js
+++ AluraBank/js/main.js
@@ -1,5 +1,6 @@
 import esUnCuil from "./ValidarCuil.js"; //Importing my function
 import esMayorDeEdad from "./validarEdad.js";
+import { tiposError, mensajes } from "./customErrors.js"; //Importing my objects
 
 const camposFormulario = document.querySelectorAll("[required]"); //Selecting and saving all elements HTML that have a required attribute 
 
@@ -12,52 +13,32 @@ camposFormulario.forEach((campo) => { //Itereating each element and adding them 
     })
 });
 
-const tiposError = [ 
-    "valueMissing", 
-    "typeMissmatch",
-    "patterMissmatch",  
-    "tooShort",
-    "customError"
-]
-
-const mensajes = {
-    nombre: {
-      valueMissing: "El campo nombre no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un nombre válido.",
-      tooShort: "Por favor, ingrese un nombre válido.",
-    },
-    email: {
-      valueMissing: "El campo email no puede estar vacío.",
-      typeMismatch: "Por favor, ingrese un email válido.",
-      tooShort: "Por favor, ingrese un e-mail válido.",
-    },
-    identificacion: {
-      valueMissing: "El campo identificación no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un número de identificación válido.",
-      tooShort: "El campo no tiene caracteres suficientes.",
-    },
-    cuil: {
-      valueMissing: "El campo cuil/cuit no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un cuil/cuit válido.",
-      customError: "El cuil/cuit ingresado no existe.",
-      tooShort: "El campo no tiene caracteres suficientes.",
-    },
-    fecha_nacimiento: {
-      valueMissing: "El campo fecha nacimiento no puede estar vacío.",
-      customError: "Debes ser mayor de 18 años para registrarte.",
-    },
-    terminos: {
-      valueMissing: "Debes aceptar los términos antes de continuar.",
-    },
-  };
-
 function verificarCampo (campo){ //Function asociated to the event
+
+    let mensaje = "";
+
     if(campo.name == "cuil" && campo.value.length >= 11){
         esUnCuil(campo)
     }
     if(campo.name == "fecha_nacimiento" && campo.value != ""){
         esMayorDeEdad(campo)
     }
+    //console.log(campo.validity) //To know properties of validation in my forms inputs
 
-    console.log(campo.validity) //To know properties of validation in my forms inputs
+    tiposError.forEach((error) => {
+        if(campo.validity[error]){
+            mensaje = mensajes[campo.name][error]; //Accesing to my customise error message based on validity properties
+
+            console.log(mensaje);
+        }
+    })
+
+    const mensajeError = campo.parentNode.querySelector(".mensaje-error");
+    const validarInputCheck = campo.checkValidity(); //
+
+    if(!validarInputCheck){
+        mensajeError.textContent = mensaje;
+    }else {
+        mensajeError.textContent = ""
+    }
 }
