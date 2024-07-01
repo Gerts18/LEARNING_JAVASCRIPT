6084ccb783a5150b9c9b791f7898b9549da3b239
German <germants100@gmail.com>
2024-07-01 11:54:16 -0600

    Customising errors

--- AluraBank/js/main.js
+++ AluraBank/js/main.js
@@ -12,6 +12,45 @@ camposFormulario.forEach((campo) => { //Itereating each element and adding them 
     })
 });
 
+const tiposError = [ 
+    "valueMissing", 
+    "typeMissmatch",
+    "patterMissmatch",  
+    "tooShort",
+    "customError"
+]
+
+const mensajes = {
+    nombre: {
+      valueMissing: "El campo nombre no puede estar vacío.",
+      patternMismatch: "Por favor, ingrese un nombre válido.",
+      tooShort: "Por favor, ingrese un nombre válido.",
+    },
+    email: {
+      valueMissing: "El campo email no puede estar vacío.",
+      typeMismatch: "Por favor, ingrese un email válido.",
+      tooShort: "Por favor, ingrese un e-mail válido.",
+    },
+    identificacion: {
+      valueMissing: "El campo identificación no puede estar vacío.",
+      patternMismatch: "Por favor, ingrese un número de identificación válido.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+    cuil: {
+      valueMissing: "El campo cuil/cuit no puede estar vacío.",
+      patternMismatch: "Por favor, ingrese un cuil/cuit válido.",
+      customError: "El cuil/cuit ingresado no existe.",
+      tooShort: "El campo no tiene caracteres suficientes.",
+    },
+    fecha_nacimiento: {
+      valueMissing: "El campo fecha nacimiento no puede estar vacío.",
+      customError: "Debes ser mayor de 18 años para registrarte.",
+    },
+    terminos: {
+      valueMissing: "Debes aceptar los términos antes de continuar.",
+    },
+  };
+
 function verificarCampo (campo){ //Function asociated to the event
     if(campo.name == "cuil" && campo.value.length >= 11){
         esUnCuil(campo)
